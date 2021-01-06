2b744f097e896247f34d132586a78ff100578aba
diego12 <diego.anchillo@tecsup.edu.pe>
2021-01-05 23:24:02 -0500

    Agregar archivos de proyecto.

---
+++ MVCAJAX/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MVCAJAX.Models;
+
+namespace MVCAJAX.Controllers
+{
+    public class StudentController : Controller
+    {
+        Proxy.StudentProxy proxy = new Proxy.StudentProxy();
+        public ActionResult IndexRazor()
+        {
+            var response = Task.Run(() => proxy.GetStudentsAsync());
+            return View(response.Result.listado);
+        }
+        public JsonResult GetStudent(string id)
+        {
+            var response = Task.Run(() => proxy.GetStudentsAsync());
+            return Json(response.Result.listado, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult createStudent(StudentModel std)
+        {
+            var response = Task.Run(() => proxy.InsertAsync(std));
+            string message = response.Result.Mensaje;
+            return Json(new { Message = message,JsonRequestBehavior.AllowGet});
+        }
+        public ActionResult UpdateStudent(StudentModel std)
+        {
+            var response = Task.Run(() => proxy.UpdateAsync(std));
+            string message = response.Result.Mensaje;
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
